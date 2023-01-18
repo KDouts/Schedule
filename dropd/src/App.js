129e4097f7518cc6c16804eb21310d118dfed79d
Kurtis <kcd257@yahoo.com>
2023-01-18 13:10:14 -0800

    added date and time

---
+++ dropd/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './App.css';
+import DateTime from './DateTime/DateTime';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World!</p>
+      <DateTime />
+    </div>
+  );
+}
+
+export default App;
